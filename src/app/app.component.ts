5ab83b2a86bd842850d770f151569a61e269b35c
RavindhiranG <ravindhirang@gmail.com>
2024-04-24 16:42:08 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TwoWayBinding';
+
+  // Two-way binding combines property binding [] with event binding () thays [()] is used for twoway binding
+  // Angular's two-way binding syntax is a combination of square brackets and parentheses, [()]. The [()] syntax combines the brackets of property binding, [], with the parentheses of event binding, ()
+  
+  name = "";
+
+  
 }
